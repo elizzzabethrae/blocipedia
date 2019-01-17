a4a19cdc24175adc7aad59975ef8fc31b42632ee
elizzzabethrae <elizabethwarners@gmail.com>
2019-01-16 20:04:04 -0500

    working on user signup view

---
+++ src/routes/users.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+const userController = require("../controllers/userController");
+
+router.get("/users/signup", userController.signUp);
+router.post("/users/signUp", userController.create);
+router.get("/users/sign_in", userController.signInForm);
+router.post("/users/sign_in", userController.signUp);
+router.get("/users/sign_out", userController.signOut);
+
+
+module.exports = router;
