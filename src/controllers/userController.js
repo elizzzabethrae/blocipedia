a418cbf76d98075c9e08b1e1f0eb282e4cb94bc5
elizzzabethrae <elizabethwarners@gmail.com>
2019-01-16 21:37:27 -0500

    working on sign up, getting errors

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -17,11 +17,11 @@ module.exports = {
 
      sgMail.setApiKey(process.env.SENDGRID_API_KEY);
      const msg = {
-       to: 'emilyspieler1@gmail.com',
+       to: 'elizabethwarners@gmail.com',
        from: 'test@example.com',
-       subject: 'Sending with SendGrid is Fun',
-       text: 'and easy to do anywhere, even with Node.js',
-       html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+       subject: 'i love bread',
+       text: 'especially homemade bread',
+       html: '<strong>especially homemade bread</strong>',
      };
      console.log(process.env.SENDGRID_API_KEY)
      sgMail.send(msg);
